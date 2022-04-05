1e0f4d3233f458cc6fc91dc8f996f78adecb03e7
edimardaros <edimar.daros@gmail.com>
2022-04-04 22:03:48 -0300

    W8D2 Autenticação: Registrando o usuário

--- javascript_note_api/app/models/user.js
+++ javascript_note_api/app/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -7,5 +9,21 @@ let userSchema = new mongoose.Schema({
   created_at: {type: Date, default: Date.now},
   updated_at: {type: Date, default: Date.now},
 });
+
+userSchema.pre('save', function(next) { // next go to the next middleware
+  if (this.isNew || this.isModified('password')) {
+    // const document = this;
+    bcrypt.hash(this.password, 10,
+      (err, hashedPassword) => {
+        if (err) 
+          next(err)
+        else {
+          this.password = hashedPassword;
+          next();
+        }
+      }  
+    )
+  }
+})
 
 module.exports = mongoose.model('User', userSchema);
